8105aa3ff89f95889b12cd1d67d414cb3e8fb384
exoks <exoks.team@gmail.com>
2025-07-18 16:49:47 +0100

    ai: Seaborn: test: distribution_plots.py > added

---
+++ ai/ml/Seaborn/test/distribution_plots.py
@@ -0,0 +1,57 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀   𓐓  distribution_plots.py 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀       Dev: oezzaou </var/spool/mail/oezzaou>
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/07/18 16:09:32 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/07/18 16:49:02 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+# ===[ Imports: ]==============================================================
+import numpy as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# ===[ main: ]=================================================================
+
+
+def main():
+    # get all available datasets in seaborn library
+    names = sns.get_dataset_names()
+    print(f"names: {names}")
+    # Load a built-in 'tips' dataset from seaborn in a DataFrame
+    tips = sns.load_dataset('tips')
+    # Explore the dataset
+    print(tips.head())
+    tips.info()
+    # Data is Clean so we are goint to visualize the dataset
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # nd_tips = tips['tip'].values
+    # print(f"nd_tips: {nd_tips[:20]}")
+    # Using matplotlib.hist
+    # ax.hist(nd_tips)
+    # Using Seaborn histogram
+    # 1. distplot() (univariate), Visualize a single distribution
+    # sns.distplot(tips['tip'], kde=False)
+    # 2. jointplot() (bivariate): It does compare two distribution
+    # sns.jointplot(x='tip', y='total_bill', data=tips)
+    # 3. kdeplot() (both): It does use kernal density estimation instead of
+    # histogram
+    # sns.kdeplot(tips['tip'])
+    # 4. pairplot(): visualize all pair of distribution in form of grid
+    # sns.pairplot(tips)
+    # 5. sns.rugplot(): a complement plot that show a distribution in form of
+    # ticks
+    sns.rugplot(tips.tip)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()

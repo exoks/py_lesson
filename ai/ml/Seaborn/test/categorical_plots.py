14dfdcf21038f3a1cd29b9c9e85d76c772938755
exoks <exoks.team@gmail.com>
2025-07-19 00:08:08 +0100

    ai: Seaborn: test: categorical_plots.py > done

---
+++ ai/ml/Seaborn/test/categorical_plots.py
@@ -0,0 +1,61 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀    𓐓  categorical_plots.py 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀       Dev: oezzaou </var/spool/mail/oezzaou>
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/07/18 18:10:59 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/07/19 00:07:36 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+# ===[ Imports: ]==============================================================
+# import numpy as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# ===[ main: ]=================================================================
+
+
+def main():
+    # get all available datasets in seaborn library
+    names = sns.get_dataset_names()
+    print(f"names: {names}")
+    # Load a built-in 'tips' dataset from seaborn in a DataFrame
+    tips = sns.load_dataset('tips')
+    # Explore the dataset
+    print(tips.head())
+    # count how many men and female in my dataset
+    tips.info()
+    # Filtering
+    male_count = tips[tips['sex'].str.contains('Male')].shape[0]
+    female_count = tips[tips['sex'].str.contains('Female')].shape[0]
+    print(f"male_count: {male_count}")
+    print(f"female_count: {female_count}")
+    # 1. barplot()
+    # visualizing the category based on estimator (mean, std, median)
+    # NOTE:> mean is the default
+
+    # sns.barplot(x='sex', y='tip', data=tips, estimator='median')
+    # 2.countplot(): show the count of categorical data
+    # sns.countplot(x='sex', data=tips)
+
+    # 3. boxplot()
+    # sns.boxplot(x='day', y='tip', data=tips, hue='sex')
+    # 4. violinplot()
+    # sns.violinplot(x='sex', y='tip', data=tips)
+
+    # 5.stripplot()
+    # sns.stripplot(x='sex', y='tip', data=tips)
+
+    # 6. swarmplot()
+    sns.swarmplot(x='sex', y='tip', data=tips)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()

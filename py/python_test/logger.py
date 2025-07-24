f143e2943bba54e6411ad0a5554dd6262f4b0b56
exoks <exoks.team@gmail.com>
2025-07-24 22:06:11 +0100

    py: python_test: logger.py > added

---
+++ py/python_test/logger.py
@@ -0,0 +1,53 @@
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀               𓐓  logger.py 𓐔           
+#  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀
+#  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀       Dev: oezzaou </var/spool/mail/oezzaou>
+#  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀
+#  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+#  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/07/24 19:42:26 by oezzaou
+#  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/07/24 22:04:48 by oezzaou
+#  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀
+#  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀
+#  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪
+#  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃
+#  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+# ===[ Imports: ]==============================================================
+import logging
+
+
+# ===[ main: ]=================================================================
+def main():
+    # 1|> Using the Root Logger (Default logger)
+    logging.basicConfig(
+        level="WARNING",
+        filename="app.log",
+        filemode='w',  # 'w' overrides the log file each time we run the code
+        format="[%(levelname)s][%(asctime)s][%(message)s]"
+    )
+
+    logging.debug("this a debug message")
+    logging.info("this an info message")
+    logging.warning("this is a warning message")
+    logging.error("this is an error message")
+
+    # 2|> Creating a Custom logger
+    logger = logging.getLogger(__name__)
+    print(__name__)
+    # creating a handler for the custom logger (logger)
+    handler = logging.FileHandler("custom.log")
+    # creating a formatter for the handler
+    formatter = logging.Formatter("[%(levelname)s][%(asctime)s][%(message)s]")
+    # add formatter to handler
+    handler.setFormatter(formatter)
+    # add handler to logger
+    logger.addHandler(handler)
+
+    logger.debug("this a debug message")
+    logger.info("this an info message")
+    logger.warning("this is a warning message")
+    logger.error("this is an error message")
+
+
+if __name__ == '__main__':
+    main()
